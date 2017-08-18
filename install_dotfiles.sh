05ecca9d53c8011899d323afff9db36c2ef0c9cf
mattidibi <matthewdibi@gmail.com>
2017-08-18 13:40:55 +0200

    Added file browser to rofi

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -7,4 +7,5 @@ cat space-vim-dark.vim > ~/.vim/colors/space-vim-dark.vim
 cat config.vim > ~/.vimrc
 cat config.tmux > ~/.tmux.conf
 cat config.rofi > ~/.Xresources
+
 echo "Installed config file. Enjoy =D"
