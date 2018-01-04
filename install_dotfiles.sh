d1ecbcb4f80e1b0d679549c3ea748578040a55ec
mattidibi <matthewdibi@gmail.com>
2018-01-04 10:27:27 +0100

    Added i3-wm configuration files
    Added i3 configuration file. Added i3status configuration file.

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,15 +1,24 @@
 if ! [ -d  ~/.vim/colors ]; then
+  echo "Creating ~/.vim/colors directory..."
   mkdir ~/.vim/colors 
 fi
 
 if ! [ -d  ~/.vim/minisnip ]; then
+  echo "Creating ~/.vim/minisnip directory..."
   mkdir ~/.vim/minisnip 
+fi
+
+if ! [ -d  ~/.i3 ]; then
+  echo "Creating ~/.i3 directory..."
+  mkdir ~/.i3
 fi
 
 cat space-vim-dark.vim > ~/.vim/colors/space-vim-dark.vim
 
 cat config.vim > ~/.vimrc
 cat config.tmux > ~/.tmux.conf
+cat config.i3 > ~/.i3/config
+cat config.i3status > ~/.i3status.conf
 
 cp minisnip/* ~/.vim/minisnip/
 
