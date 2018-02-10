29d056e97083d2aa6e381494d4920e60c9402c89
mattidibi <matthewdibi@gmail.com>
2018-02-10 12:35:03 +0100

    Added options for installation script

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,3 +1,41 @@
+#! /bin/bash
+
+# Variables
+full_install=false
+
+# Usage info
+show_help() {
+cat << EOF
+Usage: ${0##*/} [-hf] ...
+Dotfile installation script. It installs the dotfile
+inside this repository.
+
+    -h          display this help and exit.
+    -f          FULL install: it trigger vim plugin
+                installation/update through Vundle.
+EOF
+}
+
+# Option parsing
+while getopts ":fh" opt; do
+  case $opt in
+    f)
+      full_install=true
+      ;;
+    h)
+      show_help
+      exit 0
+      ;;
+    *)
+      show_help >&2
+      exit 1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
 # Checking requirements
 command -v git >/dev/null 2>&1 || { echo >&2 "I require git but it's not installed.  Aborting."; exit 1; }
 
@@ -7,7 +45,7 @@ command -v i3       >/dev/null 2>&1 || { echo >&2 "WARNING: i3-wm is not install
 command -v i3status >/dev/null 2>&1 || { echo >&2 "WARNING: i3status is not installed."; }
 
 if ! [ -e ~/.vim/bundle/Vundle.vim ]; then
-  echo "Installing Vundle plugin manager.."
+  echo "Installing Vundle plugin manager..."
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 echo "Vundle: OK"
@@ -30,9 +68,16 @@ cat config.tmux     > ~/.tmux.conf
 cat config.i3       > ~/.i3/config
 cat config.i3status > ~/.i3status.conf
 cat config.bash     > ~/.bashrc
-
 cp minisnip/* ~/.vim/minisnip/
+
+# Full install management
+if [ "$full_install" = true ] ; then
+    echo "Plugin update..."
+    vim +PluginUpdate +qall
+else
+    echo "Run ':PluginUpdate' inside vim to complete setup."
+fi
 
 echo ""
 echo "Installation complete!"
-echo "Run ':PluginUpdate' inside vim to complete setup."
+# End of file
