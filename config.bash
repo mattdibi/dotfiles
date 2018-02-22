3510f6774e65285c02a80ba6d015e7563bc7b3ca
Mattia Dal Ben <matthewdibi@gmail.com>
2018-02-22 09:06:35 +0100

    Added safe delete and better "nope" command

--- config.bash
+++ config.bash
@@ -123,6 +123,18 @@ alias lsl='ls -l'
 alias desk='cd /home/mattia/Scrivania'
 alias pcn='cd /home/mattia/Scrivania/pcn_v2/PCN'
 alias xpn='xdg-open'
-alias nope='nautilus .'
+alias nope='nautilus . & disown'
 alias dotf='cd /home/mattia/Scrivania/dotfiles'
 alias new='gnome-terminal & disown'
+
+# Safe rm
+alias rm="echo Use 'del', or the full path i.e. '/bin/rm'"
+
+TRASHPATH=/home/mattia/.local/share/Trash/files
+function del()
+{
+for i in $*
+do
+mv $i $TRASHPATH/$i
+done
+}
