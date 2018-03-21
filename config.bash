1958280e260f5e757ec88ca6be22d8a3e08fd0e5
Mattia Dal Ben <matthewdibi@gmail.com>
2018-03-21 12:18:23 +0100

    Short path for colorless prompt

--- config.bash
+++ config.bash
@@ -59,7 +59,7 @@ fi
 if [ "$color_prompt" = yes ]; then
     PS1='${debian_chroot:+($debian_chroot)}[\[\033[01;32m\]\u@\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]]\$ '
 else
-    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+    PS1='${debian_chroot:+($debian_chroot)}\u@:\W\$ '
 fi
 unset color_prompt force_color_prompt
 
