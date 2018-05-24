e59f7c897e913055a9407b3360e407b3a3ab416b
Mattia Dal Ben <matthewdibi@gmail.com>
2018-05-24 13:51:54 +0200

    Fixed syntax for plugins

--- config.zsh
+++ config.zsh
@@ -58,10 +58,7 @@ ZSH_THEME="agnoster"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(
-  git,
-  vi-mode
-)
+plugins=( git vi-mode )
 
 source $ZSH/oh-my-zsh.sh
 
