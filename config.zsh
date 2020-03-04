16433dce18a2c7425f127518210ccba42075e907
Mattia Dal Ben <matthewdibi@gmail.com>
2020-03-04 09:38:20 +0100

    Single-command sonar build

--- config.zsh
+++ config.zsh
@@ -116,5 +116,5 @@ prompt_context() {
 
 # Sonar aliases
 # WARNING: Auth token is embedded here
-# alias sonar-build='/opt/build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir bw-output make clean all -j8'
+# alias sonar-build='/opt/build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir bw-output make clean all -j8 && make test && ctest -T coverage'
 # alias sonar-scan='/opt/sonar-scanner-4.2.0.1873-linux/bin/sonar-scanner -Dsonar.host.url=https://sonarcloud.io -Dsonar.projectKey=<PROJECT_KEY> -Dsonar.organization=<ORGANIZATION> -Dsonar.login=<TOKEN> -Dsonar.cfamily.build-wrapper-output=build/bw-output -Dsonar.cfamily.threads=8 -Dsonar.sources=src -Dsonar.tests=test -Dsonar.projectBaseDir=./ -Dsonar.cfamily.gcov.reportsPath=build/Testing/CoverageInfo -Dsonar.branch.name=<BRANCH_NAME>'
