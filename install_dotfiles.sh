b939b8971f19ee00c5fbd6050415c9479b40ae52
Mattia Dal Ben <matthewdibi@gmail.com>
2020-03-15 10:32:40 +0100

    Added target to installation script

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -2,22 +2,29 @@
 
 # Variables
 full_install=false
+target="all"
 
 # Usage info
 show_help() {
 cat << EOF
-Usage: ${0##*/} [-hf] ...
+Usage: ${0##*/} [-t target] [-hf] ...
 Dotfile installation script. It installs the dotfile
 inside this repository.
 
     -h          display this help and exit.
+    -t          TARGET: selects which application to
+                update. Possibile targets:
+                    - "all" (default)
+                    - "i3"
+                    - "zsh"
+                    - "vim"
     -f          FULL install: it trigger vim plugin
                 installation/update through Vundle.
 EOF
 }
 
 # Option parsing
-while getopts ":fh" opt; do
+while getopts ":t:fh" opt; do
   case $opt in
     f)
       full_install=true
@@ -25,6 +32,9 @@ while getopts ":fh" opt; do
     h)
       show_help
       exit 0
+      ;;
+    t)
+      target=${OPTARG}
       ;;
     *)
       show_help >&2
@@ -35,6 +45,11 @@ while getopts ":fh" opt; do
       ;;
   esac
 done
+
+if ! [[ "$target" = "all" || "$target" = "i3" || "$target" = "zsh" || "$target" = "vim" ]]; then
+    echo "Uknown target \"$target\". Abort."
+    exit 1
+fi
 
 # Checking requirements
 command -v git >/dev/null 2>&1 || { echo >&2 "I require git but it's not installed.  Aborting."; exit 1; }
@@ -66,10 +81,22 @@ fi
 echo "i3-wm: OK"
 
 # Dotfiles installation
-cp -r custom_snippets/ ~/.vim/
-cat config.vim      > ~/.vimrc
-cat config.i3       > ~/.config/regolith/i3/config
-cat config.zsh      > ~/.zshrc
+if [[ "$target" = "vim" ||  "$target" = "all" ]]; then
+    echo "Installing ~/.vimrc"
+    cat config.vim      > ~/.vimrc
+    echo "Installing custom snippets"
+    cp -r custom_snippets/ ~/.vim/
+fi
+
+if [[ "$target" = "i3" ||  "$target" = "all" ]]; then
+    echo "Installing ~/.config/regolith/i3/config"
+    cat config.i3       > ~/.config/regolith/i3/config
+fi
+
+if [[ "$target" = "zsh" ||  "$target" = "all" ]]; then
+    echo "Installing ~/.zshrc"
+    cat config.zsh      > ~/.zshrc
+fi
 
 # Full install management
 if [ "$full_install" = true ] ; then
