64c7471e3d48db26dfd38dde6bccf91b5d092216
Mattia Dal Ben <matthewdibi@gmail.com>
2020-11-28 14:46:14 +0100

    Added updater script

---
+++ update_nvim.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+CURRENT_EXE=/usr/bin/nvim
+BACKUP_EXE=/usr/bin/old.nvim
+
+echo "NeoVIM Appimage updater"
+
+echo "Updating nvim.appimage"
+read -s -p "Enter Password for sudo: " sudoPW
+
+echo "Downloading..."
+curl -sSLO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+chmod u+x nvim.appimage
+
+NEW_VER="$(./nvim.appimage --version | head -n 1)"
+echo "Downloaded version: $NEW_VER"
+
+# Check if NeoVIM is already installed
+if [[ -f "$CURRENT_EXE" ]]; then
+    CURRENT_VER="$($CURRENT_EXE --version | head -n 1)"
+    echo "Currently installed version: $CURRENT_VER"
+
+    # Check if versions match
+    if [ "$NEW_VER" = "$CURRENT_VER" ]; then
+        echo "Most recent version is already installed. Quitting..."
+        rm nvim.appimage
+        exit 0
+    fi
+
+    # Backup older version
+    echo "Backing up current version to $BACKUP_EXE."
+    echo $sudoPW | sudo cp nvim.appimage $BACKUP_EXE
+else
+    echo "No NeoVIM binary found in $CURRENT_EXE."
+fi
+
+echo "Installing binary to $CURRENT_EXE"
+echo $sudoPW | sudo mv nvim.appimage $CURRENT_EXE
+
+echo "Update complete."
