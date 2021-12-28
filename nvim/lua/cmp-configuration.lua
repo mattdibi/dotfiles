00727eade0717f623f24b726edd6f68a2c52141d
Mattia Dal Ben <matthewdibi@gmail.com>
2021-12-28 11:39:59 +0100

    Roll-back to default completion

--- nvim/lua/cmp-configuration.lua
+++ nvim/lua/cmp-configuration.lua
@@ -12,7 +12,7 @@ cmp.setup({
     },
     sources = {
         { name = 'nvim_lsp',  max_item_count = 10 },
-        { name = 'ultisnips', max_item_count =  5 , keyword_length = 1 },
+        { name = 'ultisnips', max_item_count =  5 },
         { name = 'buffer',    max_item_count =  5 , keyword_length = 4 },
         { name = 'path',      max_item_count =  5 },
     }
