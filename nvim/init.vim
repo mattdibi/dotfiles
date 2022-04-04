d585f86c3d5f888d122632c3edc1b6f970b0be17
Mattia Dal Ben <matthewdibi@gmail.com>
2022-04-04 22:34:08 +0200

    Initial commit (set colorscheme)

---
+++ nvim/init.vim
@@ -0,0 +1,7 @@
+lua require('plugins')
+
+if has('nvim') || has('termguicolors')
+  set termguicolors
+endif
+
+colorscheme challenger_deep
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,9 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Eye-candy plugins
+  use 'challenger-deep-theme/vim'
+end)
