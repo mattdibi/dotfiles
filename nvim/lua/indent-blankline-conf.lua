6e19c83bffa86992235b5d1ad1ee73a371388634
Mattia Dal Ben <matthewdibi@gmail.com>
2022-04-12 21:13:31 +0200

    Improved readability

--- nvim/lua/indent-blankline-conf.lua
+++ nvim/lua/indent-blankline-conf.lua
@@ -2,8 +2,21 @@
 
 require("indent_blankline").setup {
     char = "▏",
-    buftype_exclude = {"terminal", "nofile"},
-    filetype_exclude = {"yaml", 'dashboard', 'git', 'markdown', 'text', 'terminal', 'lspinfo', 'packer', 'NvimTree'},
+    buftype_exclude = {
+        "terminal",
+        "nofile"
+    },
+    filetype_exclude = {
+        "yaml",
+        'dashboard',
+        'git',
+        'markdown',
+        'text',
+        'terminal',
+        'lspinfo',
+        'packer',
+        'NvimTree'
+    },
     show_first_indent_level = false,
     show_trailing_blankline_indent = false
 }
