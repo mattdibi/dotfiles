cd5d3f6eadf182a8af970251b8eed52c999446d3
Mattia Dal Ben <matthewdibi@gmail.com>
2022-04-22 23:11:42 +0200

    Moved autocmds in their own lua file

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ require('plugins')
 require('settings')
 require('keybindings')
 require('commands')
+require('autocmds')
 
 -- Plugin configuration
 require('treesitter-conf')
@@ -10,32 +11,6 @@ require('indent-blankline-conf')
 require('telescope-conf')
 require('lualine-conf')
 require('gitsigns-conf')
-
--- Highlight on yank
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-    callback = function()
-        vim.highlight.on_yank({higroup="Visual"})
-    end,
-    group = highlight_group,
-    pattern = '*',
-})
-
--- Go to last loc when opening a buffer
-vim.api.nvim_create_autocmd("BufRead", {
-    command = [[call setpos(".", getpos("'\""))]]
-})
-
--- Terminal visual tweaks
-local termGrp = vim.api.nvim_create_augroup("terminal_job", { clear = true })
-vim.api.nvim_create_autocmd("TermOpen", {
-    command = "startinsert",
-    group = termGrp,
-})
-vim.api.nvim_create_autocmd("TermOpen", {
-    command = "setlocal listchars= nonumber norelativenumber",
-    group = termGrp,
-})
 
 -- Coloscheme
 vim.cmd [[
---
+++ nvim/lua/autocmds.lua
@@ -0,0 +1,26 @@
+-- Highlight on yank
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+        vim.highlight.on_yank({higroup="Visual"})
+    end,
+    group = highlight_group,
+    pattern = '*',
+})
+
+-- Go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufRead", {
+    command = [[call setpos(".", getpos("'\""))]]
+})
+
+-- Terminal visual tweaks
+local termGrp = vim.api.nvim_create_augroup("terminal_job", { clear = true })
+vim.api.nvim_create_autocmd("TermOpen", {
+    command = "startinsert",
+    group = termGrp,
+})
+vim.api.nvim_create_autocmd("TermOpen", {
+    command = "setlocal listchars= nonumber norelativenumber",
+    group = termGrp,
+})
+
