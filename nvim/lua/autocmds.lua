8afdaded6be56b359d389643ebbcdcc71135a34c
Mattia Dal Ben <matthewdibi@gmail.com>
2022-04-22 23:29:27 +0200

    Handle Jenkinsfile syntax highlight

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -20,6 +20,12 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     end,
 })
 
+-- Fix text highligh for Jenkinsfiles
+vim.api.nvim_create_autocmd( "BufEnter", {
+    pattern = {"Jenkinsfile"},
+    command = "set filetype=groovy"
+})
+
 -- Terminal visual tweaks
 local terminal_group = vim.api.nvim_create_augroup("terminal_job", { clear = true })
 vim.api.nvim_create_autocmd("TermOpen", {
