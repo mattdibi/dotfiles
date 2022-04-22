7c36cbe325c6d5a9cdc4ac1a49ae928f4bfacfb7
Mattia Dal Ben <matthewdibi@gmail.com>
2022-04-22 23:08:46 +0200

    Fixed terminal visual tweaks autocmd style for consistency

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,36 +22,22 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- Go to last loc when opening a buffer
-vim.api.nvim_create_autocmd(
-  "BufRead",
-  {command = [[call setpos(".", getpos("'\""))]]}
-)
+vim.api.nvim_create_autocmd("BufRead", {
+    command = [[call setpos(".", getpos("'\""))]]
+})
+
+-- Terminal visual tweaks
+local termGrp = vim.api.nvim_create_augroup("terminal_job", { clear = true })
+vim.api.nvim_create_autocmd("TermOpen", {
+    command = "startinsert",
+    group = termGrp,
+})
+vim.api.nvim_create_autocmd("TermOpen", {
+    command = "setlocal listchars= nonumber norelativenumber",
+    group = termGrp,
+})
 
 -- Coloscheme
 vim.cmd [[
     colorscheme challenger_deep
 ]]
-
--- Autocommands
---- This function is taken from https://github.com/norcalli/nvim_utils
-function nvim_create_augroups(definitions)
-    for group_name, definition in pairs(definitions) do
-        vim.api.nvim_command('augroup '..group_name)
-        vim.api.nvim_command('autocmd!')
-        for _, def in ipairs(definition) do
-            local command = table.concat(vim.tbl_flatten{'autocmd', def}, ' ')
-            vim.api.nvim_command(command)
-        end
-        vim.api.nvim_command('augroup END')
-    end
-end
-
-local autocmds = {
-    terminal_job = {
-        { "TermOpen", "*", "startinsert" };
-        { "TermOpen", "*", "setlocal listchars= nonumber norelativenumber" };
-    };
-}
-
-nvim_create_augroups(autocmds)
--- Autocommands END
