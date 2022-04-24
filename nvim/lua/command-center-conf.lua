ac269661b66ee78c25ec72d174c5e76bd5780f52
Mattia Dal Ben <matthewdibi@gmail.com>
2022-04-24 19:17:43 +0200

    Improved command descriptions

--- nvim/lua/command-center-conf.lua
+++ nvim/lua/command-center-conf.lua
@@ -4,36 +4,36 @@ local noremap = {noremap = true}
 command_center.add({
     -- Command palette
     {
-        description = "Open command center",
+        description = "[Comm] Open command center",
         cmd = "<CMD>Telescope command_center<CR>",
         keybindings = { "n", "<leader>fc", noremap },
     },  {
     -- Telescope finders
-        description = "Find project files",
+        description = "[Find] Find project files",
         cmd = "<CMD>lua require('telescope-conf').project_files()<CR>",
         keybindings = { "n", "<C-p>", noremap },
     },  {
-        description = "Find inside current buffer",
+        description = "[Find] Find inside current buffer",
         cmd = "<CMD>Telescope current_buffer_fuzzy_find<CR>",
         keybindings = { "n", "<leader>fl", noremap },
     }, {
-        description = "Find string in project",
+        description = "[Find] Find string in project",
         cmd = "<CMD>Telescope live_grep<CR>",
         keybindings = { "n", "<leader>fg", noremap },
     }, {
-        description = "Find hidden files",
+        description = "[Find] Find hidden files",
         cmd = "<CMD>Telescope find_files hidden=true<CR>",
     }, {
-        description = "Find help",
+        description = "[Find] Find help",
         cmd = "<CMD>Telescope help_tags<CR>",
         keybindings = { "n", "<leader>fh", noremap },
     }, {
     -- LSP
-        description = "Get LSP log",
+        description = "[LSP] Get LSP log",
         cmd = "<CMD>lua vim.cmd('e'..vim.lsp.get_log_path())<CR>",
     }, {
     -- Functions
-        description = "Strip trailing whitespaces",
+        description = "[Space] Strip trailing whitespaces",
         cmd = "<CMD>StripWhitespace<CR>",
     }
 })
