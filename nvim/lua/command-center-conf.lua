21042640241b563282ab4cb3e37292012bf35791
Mattia Dal Ben <matthewdibi@gmail.com>
2022-04-24 10:07:01 +0200

    Removed unnecessary from command-center

--- nvim/lua/command-center-conf.lua
+++ nvim/lua/command-center-conf.lua
@@ -1,6 +1,5 @@
 local command_center = require("command_center")
 local noremap = {noremap = true}
-local silent_noremap = {noremap = true, silent = true}
 
 command_center.add({
     -- Command palette
@@ -17,17 +16,9 @@ command_center.add({
         description = "Find inside current buffer",
         cmd = "<CMD>Telescope current_buffer_fuzzy_find<CR>",
         keybindings = { "n", "<leader>fl", noremap },
-    },  {
-        description = "Find files",
-        cmd = "<CMD>Telescope find_files<CR>",
-        keybindings = { "n", "<leader>ff", noremap },
     }, {
         description = "Find hidden files",
         cmd = "<CMD>Telescope find_files hidden=true<CR>",
-    }, {
-        description = "Find buffers",
-        cmd = "<CMD>Telescope buffers<CR>",
-        keybindings = { "n", "<leader>fb", noremap },
     }, {
         description = "Find help",
         cmd = "<CMD>Telescope help_tags<CR>",
