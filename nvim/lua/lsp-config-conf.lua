ea2cf20915a45ac1297c1e4e966a2ce74d9976d3
Mattia Dal Ben <matthewdibi@gmail.com>
2022-04-24 17:38:57 +0200

    Added nvim cmp capabilites inside LSP config

--- nvim/lua/lsp-config-conf.lua
+++ nvim/lua/lsp-config-conf.lua
@@ -28,9 +28,14 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local servers = { 'clangd' }
+-- nvim-cmp configuration
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
+    capabilities = capabilities,
     flags = {
       -- This will be the default in neovim 0.7+
       debounce_text_changes = 150,
