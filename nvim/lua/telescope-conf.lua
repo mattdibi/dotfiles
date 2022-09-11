33603a2dd57bbb29a23c76d0c8bde3bf5f11a0ae
Mattia Dal Ben <matthewdibi@gmail.com>
2022-09-11 17:17:04 +0200

    Ignore images in find file

--- nvim/lua/telescope-conf.lua
+++ nvim/lua/telescope-conf.lua
@@ -1,6 +1,12 @@
 -- Telescope configuration
 local actions = require('telescope.actions')
 
+local ignore_patterns = {
+    "%.png",
+    "%.jpg",
+    "%.jpeg"
+}
+
 require('telescope').setup{
 defaults = {
     -- Set prompt position to top
@@ -21,9 +27,12 @@ defaults = {
 local M = {}
 
 M.project_files = function()
-  local opts = {} -- define here if you want to define something
-  local ok = pcall(require'telescope.builtin'.git_files, opts)
-  if not ok then require'telescope.builtin'.find_files(opts) end
+    local opts = {
+        -- define here if you want to define something
+        file_ignore_patterns = ignore_patterns
+    }
+    local ok = pcall(require'telescope.builtin'.git_files, opts)
+    if not ok then require'telescope.builtin'.find_files(opts) end
 end
 
 return M
