5188c216a354a4f5a24cb1eedd5a71fbb1003705
Mattia Dal Ben <matthewdibi@gmail.com>
2022-12-16 19:12:21 +0100

    feat: refresh treesitter configuration

--- nvim/lua/treesitter-conf.lua
+++ nvim/lua/treesitter-conf.lua
@@ -1,8 +1,16 @@
 -- Treesitter configuration
-
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "cpp", "python", "lua", "java", "rust" }, -- one of "all", "language", or a list of languages
+  -- A list of parser names, or "all"
+  ensure_installed = { "help", "cpp", "python", "lua", "java", "rust" }, -- one of "all", "language", or a list of languages
+
   highlight = {
-    enable = true           -- false will disable the whole extension
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
   }
 }
