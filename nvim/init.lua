306815ca39f51fa00afe12afa23992545689ed5c
Mattia Dal Ben <matthewdibi@gmail.com>
2022-12-24 17:13:21 +0100

    refactor: move all plugin configuration into after/plugin folder

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,19 +5,6 @@ require('keybindings')
 require('commands')
 require('autocmds')
 
--- Plugin configuration
-require('treesitter-conf')
-require('nvim-cmp-conf')
-require('command-center-conf')
-require('indent-blankline-conf')
-require('telescope-conf')
-require('lualine-conf')
-require('gitsigns-conf')
-require('lsp-config-conf')
-require('ultisnips-conf')
-require('img-paste-conf')
-require('undotree-conf')
-
 -- Coloscheme
 vim.cmd.colorscheme('challenger_deep')
 
