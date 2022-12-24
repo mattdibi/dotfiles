fb4aad94f776b366d92d64e0fe3d31a217bc940c
Mattia Dal Ben <matthewdibi@gmail.com>
2022-12-24 22:38:42 +0100

    refactor: remove use of deprecated function

--- nvim/after/plugin/lsp-config.lua
+++ nvim/after/plugin/lsp-config.lua
@@ -50,7 +50,7 @@ end
 
 -- nvim-cmp configuration
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
