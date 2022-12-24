5cd919a209ce2bf1c1d35497e437e8d7404fc97d
Mattia Dal Ben <matthewdibi@gmail.com>
2022-12-24 17:35:27 +0100

    docs: add note about how to read config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,21 @@
+--[[ Notes to people reading my configuration!
+
+Much of the configuration of individual plugins you can find in either:
+
+./after/plugin/*
+  This is where configuration for lua plugins live.
+  They get auto sourced on startup. In general, the name of the file configures
+  the plugin with the corresponding name.
+
+./lua/mattdibi/*.lua
+  This is where configuration/extensions for new style plugins live.
+  They don't get sourced automatically, but do get sourced by doing something like:
+    require('mattdibi.dap')
+  or similar. I generally recommend that people do this so that you don't accidentally
+  override any of the plugin requires with namespace clashes. So don't just put your configuration
+  in "./lua/dap.lua" because then it will override the plugin version of "dap.lua"
+
+--]]
+
 require('mattdibi')
 
