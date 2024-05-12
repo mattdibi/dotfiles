ef7662b420b397f129561cf397053fd18eaea954
Mattia Dal Ben <matthewdibi@gmail.com>
2024-05-12 14:36:19 +0200

    feat: colorscheme

--- nvim/lua/mattdibi/packer.lua
+++ nvim/lua/mattdibi/packer.lua
@@ -1,44 +1,62 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
-return require('packer').startup(function()
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-  -- Eye-candy plugins
-  use 'challenger-deep-theme/vim'
-  use 'nvim-lualine/lualine.nvim'
+require("lazy").setup({
+    spec = "mattdibi.plugins",
+    change_detection = { notify = false }
+})
 
-  -- Visual feedback
-  use 'ntpeters/vim-better-whitespace'
-  use 'lewis6991/gitsigns.nvim'
-  use 'nvim-treesitter/nvim-treesitter'
+-- return require('packer').startup(function()
+--   -- Packer can manage itself
+--   use 'wbthomason/packer.nvim'
 
-  -- Markdown
-  use 'mattdibi/incolla.nvim'
+--   -- Eye-candy plugins
+--   use 'challenger-deep-theme/vim'
+--   use 'nvim-lualine/lualine.nvim'
 
-  -- Autocompletion
-  use 'SirVer/ultisnips'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-nvim-lua'
-  use 'quangnguyen30192/cmp-nvim-ultisnips'
-  use 'github/copilot.vim'
+--   -- Visual feedback
+--   use 'ntpeters/vim-better-whitespace'
+--   use 'lewis6991/gitsigns.nvim'
+--   use 'nvim-treesitter/nvim-treesitter'
 
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/cmp-nvim-lsp'
+--   -- Markdown
+--   use 'mattdibi/incolla.nvim'
 
-  -- Navigation
-  use {
-      "nvim-telescope/telescope.nvim",
-      requires = { "nvim-lua/plenary.nvim" }
-  }
-  use 'tpope/vim-vinegar'
+--   -- Autocompletion
+--   use 'SirVer/ultisnips'
+--   use 'hrsh7th/nvim-cmp'
+--   use 'hrsh7th/cmp-path'
+--   use 'hrsh7th/cmp-buffer'
+--   use 'hrsh7th/cmp-nvim-lua'
+--   use 'quangnguyen30192/cmp-nvim-ultisnips'
+--   use 'github/copilot.vim'
 
-  -- Basics
-  use 'tpope/vim-surround'
-  use 'tpope/vim-repeat'
-  use 'tpope/vim-commentary'
-end)
+--   use 'williamboman/mason.nvim'
+--   use 'williamboman/mason-lspconfig.nvim'
+--   use 'neovim/nvim-lspconfig'
+--   use 'hrsh7th/cmp-nvim-lsp'
+
+--   -- Navigation
+--   use {
+--       "nvim-telescope/telescope.nvim",
+--       requires = { "nvim-lua/plenary.nvim" }
+--   }
+--   use 'tpope/vim-vinegar'
+
+--   -- Basics
+--   use 'tpope/vim-surround'
+--   use 'tpope/vim-repeat'
+--   use 'tpope/vim-commentary'
+-- end)
---
+++ nvim/lua/mattdibi/plugins/coloscheme.lua
@@ -0,0 +1,6 @@
+return {
+    "challenger-deep-theme/vim",
+    config = function()
+        vim.cmd("colorscheme challenger_deep")
+    end
+}
