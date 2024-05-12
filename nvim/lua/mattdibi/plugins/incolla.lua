ffb3f704132d4942b651331e83b7f1630a55abb9
Mattia Dal Ben <matthewdibi@gmail.com>
2024-05-12 15:12:26 +0200

    feat: added incolla.nvim

---
+++ nvim/lua/mattdibi/plugins/incolla.lua
@@ -0,0 +1,20 @@
+return {
+    "mattdibi/incolla.nvim",
+    config = function()
+        require("incolla").setup {
+            markdown = {
+                affix = "![](%s)",
+            },
+            tex = {
+                affix = "%s"
+            }
+        }
+
+        vim.api.nvim_set_keymap('n', '<leader>xp', '', {
+            noremap = true,
+            callback = function()
+                require'incolla'.incolla()
+            end,
+        })
+    end
+}
