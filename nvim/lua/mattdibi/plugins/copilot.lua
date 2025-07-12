82e8ab16be5a137b3ed6d12ca03f9726578a9c52
Mattia Dal Ben <matthewdibi@gmail.com>
2025-07-12 08:58:00 +0200

    feat: disable Copilot by default

--- nvim/lua/mattdibi/plugins/copilot.lua
+++ nvim/lua/mattdibi/plugins/copilot.lua
@@ -1,3 +1,8 @@
 return {
-    "github/copilot.vim"
+    "github/copilot.vim",
+    lazy = false,
+    config = function()
+      -- Disable Copilot for all filetypes by default
+      vim.g.copilot_enabled = false;
+    end
 }
