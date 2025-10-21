83d800550064e1b80a1952409cba3150a98bfbbb
Mattia Dal Ben <matthewdibi@gmail.com>
2025-10-21 21:55:56 +0200

    feat: terminal improvements

--- nvim/plugin/autocmds.lua
+++ nvim/plugin/autocmds.lua
@@ -13,19 +13,3 @@ vim.api.nvim_create_autocmd( "BufEnter", {
     pattern = {"Jenkinsfile"},
     command = "set filetype=groovy"
 })
-
--- Terminal visual tweaks
-local terminal_group = vim.api.nvim_create_augroup("terminal_job", { clear = true })
-vim.api.nvim_create_autocmd("TermOpen", {
-    command = "startinsert",
-    group = terminal_group,
-})
-vim.api.nvim_create_autocmd("TermOpen", {
-    command = "setlocal listchars= nonumber norelativenumber",
-    group = terminal_group,
-})
-vim.api.nvim_create_autocmd("TermOpen", {
-    command = "DisableWhitespace",
-    group = terminal_group,
-})
-
