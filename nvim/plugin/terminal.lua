9a0bcf6dbe42f599e60dabeca0248b77daeea8c5
Mattia Dal Ben <matthewdibi@gmail.com>
2025-10-21 21:58:09 +0200

    feat: improved shortcut for split temrinal

--- nvim/plugin/terminal.lua
+++ nvim/plugin/terminal.lua
@@ -16,7 +16,7 @@ vim.api.nvim_create_autocmd("TermOpen", {
 vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
 
 -- Open a terminal at the bottom of the screen with a fixed height.
-vim.keymap.set("n", ",st", function()
+vim.keymap.set("n", "<leader>st", function()
   vim.cmd.new()
   vim.cmd.wincmd "J"
   vim.api.nvim_win_set_height(0, 12)
