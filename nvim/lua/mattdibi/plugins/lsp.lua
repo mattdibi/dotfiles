e94c5777dad1c9e8f9cd453bb899f92896d90306
Mattia Dal Ben <matthewdibi@gmail.com>
2025-11-28 20:42:38 +0100

    fix: proper way to setup LSP shortcuts

--- nvim/lua/mattdibi/plugins/lsp.lua
+++ nvim/lua/mattdibi/plugins/lsp.lua
@@ -19,26 +19,6 @@ return {
         vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
         vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
-        -- Use an on_attach function to only map the following keys
-        -- after the language server attaches to the current buffer
-        local on_attach = function(_, bufnr)
-            -- Enable completion triggered by <c-x><c-o>
-            vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-            -- Mappings.
-            -- See `:help vim.lsp.*` for documentation on any of the below functions
-            vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-            vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader><C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-            vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-            vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-            vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-            vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-            vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-            vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>af', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-            vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ar', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-            vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>=', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
-        end
-
         -- Use a loop to conveniently call 'setup' on multiple servers and
         -- map buffer local keybindings when the language server attaches
         --
--- nvim/plugin/autocmds.lua
+++ nvim/plugin/autocmds.lua
@@ -29,3 +29,24 @@ vim.api.nvim_create_autocmd("TermOpen", {
     group = terminal_group,
 })
 
+
+-- Add LSP shortcut only when LSP attaches to the buffer
+local lspattachgroup = vim.api.nvim_create_augroup('lspattachgroup', {})
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = lspattachgroup,
+    callback = function(e)
+    local opts = { buffer = e.buf }
+
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader><C-k>", function() vim.lsp.buf.signature_help() end, opts)
+
+    vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "<leader>gD", function() vim.lsp.buf.declaration() end, opts)
+    vim.keymap.set("n", "<leader>gi", function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
+
+    vim.keymap.set("n", "<leader>af", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>ar", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<leader>=", function() vim.lsp.buf.format() end, opts)
+    end
+})
