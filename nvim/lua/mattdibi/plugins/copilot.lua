844f2d8f3b03e017e55c1d0def83e0e79196ced0
Mattia Dal Ben <matthewdibi@gmail.com>
2026-06-01 09:30:08 +0200

    feat: remove copilot (company subscription ended)

--- nvim/lua/mattdibi/plugins/copilot.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "github/copilot.vim",
-    lazy = false,
-    config = function()
-      -- Disable Copilot for all filetypes by default
-      vim.g.copilot_enabled = false;
-    end
-}
